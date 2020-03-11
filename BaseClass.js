ebe798db94b596d79797cc6b18cc7f25ead05192
Aditya8Prakash <60220908+Aditya8Prakash@users.noreply.github.com>
2020-03-11 16:49:38 +0530

    Update BaseClass.js

--- BaseClass.js
+++ BaseClass.js
@@ -2,7 +2,7 @@ class BaseClass{constructor(x,y,width,height,angle){var options={restitution:0.5
 this.body=Bodies.rectangle(x,y,width,height,options);
 this.width=width;
 this.height=height;
-this.image=loadImage("sprites/base.png");
+this.image=loadImage("base.png");
 World.add(world,this.body);}
 display(){var angle=this.body.angle;
 push();
